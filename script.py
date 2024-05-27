b8bceafaecdc9006f5aa1b9ef76c1e6c181a35e1
anthdsslll <ezembamcanthony1@gmail.com>
2024-05-27 15:03:48 +0100

    project 1

---
+++ script.py
@@ -0,0 +1,20 @@
+# Ask user for their first name
+fname = input("Please enter your first name: ")
+
+# Ask user for their last name
+lname = input("Please enter your last name: ")
+
+# Ask user for their age
+age = int(input("Please enter your age: "))
+
+# Calculate age in 10 years
+age_in_10_years = age + 10
+
+# Output user's full name and age
+print("Your full name is:", fname, lname, "and you are", age, "years old.")
+
+# Output user's first name and age in 10 years
+print("In 10 years, your first name will still be", fname, "and you will be", age_in_10_years, "years old.")
+
+
+#done
